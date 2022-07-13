98399b2f7e21c61632b8470de1b9925c866f7431
luis_mendoza <luis.mendoza.alonso@hotmail.com>
2022-07-13 15:45:14 -0500

    Agregando estilos

--- database/firebase.js
+++ database/firebase.js
@@ -13,13 +13,6 @@ var firebaseConfig = {
   storageBucket: "estacion-espacial.appspot.com",
   messagingSenderId: "1002963692173",
   appId: "1:1002963692173:web:b1e2ff684e79e066c590c4"
-
-  // apiKey: APP_FIREBASE_APIKEY,
-  //   authDomain: APP_FIREBASE_AUTHDOMAIN,
-  //   projectId: APP_FIREBASE_PROJECTID,
-  //   storageBucket: APP_FIREBASE_STORAGEBUCKET,
-  //   messagingSenderId: APP_FIREBASE_MESSAGINGSENDERID,
-  //   appId: APP_FIREBASE_APPID
 };
 
 
@@ -35,58 +28,10 @@ const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
 
+export { db, getFirestore,  addDoc, collection, getDocs, doc, updateDoc, deleteField };
 
 
 
 
-// async function insertar(){
-//   try {
-//     const docRef = await addDoc(collection(db, "users"), {
-//       first: "Alan",
-//       middle: "Mathison",
-//       last: "Turing",
-//       born: 1912
-//     });
-  
-//     console.log("Document written with ID: ", docRef.id);
-//   } catch (e) {
-//     console.error("Error adding document: ", e);
-//   }
-// }
-
-// insertar()
 
 
-// async function optenertDatos(){
-// const querySnapshot = await getDocs(collection(db, "users"));
-// querySnapshot.forEach((doc) => {
-//   console.log(`${doc.id} => ${doc.data()}`);
-// })
-// }
-// optenertDatos()
-
-async function UpdateFieldsInAdocument(){
-
-  const ref = collection(db, 'users', 'Maye'));
-  
-  await updateDoc(
-    ref,
-    {
-      born: '1234',
-      first: 'luis',
-      last: 'meendoza',
-      middle:'mate'
-      }
-  )
-  .then(()=>{
-    alert("Document actualizado");
-    
-  })  
-  .catch((error)=>{
-      alert("Document con error al actualizar: " + error);
-    
-  })
-
-}
-
-UpdateFieldsInAdocument();
--- js/index.js
+++ js/index.js
@@ -1,2 +1,31 @@
+import { db, getFirestore, addDoc, collection, getDocs, doc, updateDoc, deleteField } from '../database/firebase.js';
 
 
+async function CrearNave() {
+  try {
+    const docRef = await addDoc(collection(db, "users"), {
+      first: "Alan",
+      middle: "Mathison",
+      last: "Turing",
+      born: 1912
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
+ //CrearNave()
+
+
+
+
+//Funcion para obtener todos las naves
+export async function ListarNaves() {
+  const querySnapshot = await getDocs(collection(db, "users"));
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data()}`);
+  })
+}
+
+ListarNaves();
