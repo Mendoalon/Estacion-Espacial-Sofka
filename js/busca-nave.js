3edd29c980e1c21c153fb5fbbf084765de03e06a
luis_mendoza <luis.mendoza.alonso@hotmail.com>
2022-07-14 17:14:31 -0500

    Creando funcionalidad buscar nave. Se ajustan estilo de la aplicación.

---
+++ js/busca-nave.js
@@ -0,0 +1,35 @@
+import { db, addDoc, collection, getDocs  } from '../database/firebase.js';
+
+const cards2 = document.getElementById('card-teplate-dos');
+
+const formBuscar = document.querySelector("#form-buscar");
+ formBuscar.addEventListener("submit", async (e) => {
+ e.preventDefault();
+ const querySnapshot = await getDocs(collection(db, "naves"));
+ querySnapshot.forEach((item) => {
+if (item.data().nombre == document.querySelector("#bnombre").value) {
+  cards2.innerHTML += `
+   <div class="card">
+     <div class="card-header">
+       <h4>${item.data().nombre}</h4>
+     </div>
+     <div class="card-body">
+       <p class="text-center">Tipo: ${item.data().tipo}</p>
+       <p class="text-center">Primer Lanzamiento: ${item.data().planzamiento}</p>
+       <p class="text-center">Ultimo Lanzamiento: ${item.data().ultilanzamiento}</p>
+       <p class="text-center">Version: ${item.data().version}</p>
+       </div>
+       <div class="card-footer">
+       <a href="#" class="btn btn-sm btn-dark ">Eliminar</a>
+       <a href="#" class="btn btn-sm btn-dark ">Actualizar</a>
+       </div>
+     </div>
+     `;
+   
+
+} else {
+  
+} 
+
+ })
+})
--- js/index.js
+++ js/index.js
@@ -1,32 +1,33 @@
-import { db, getFirestore, addDoc, collection, getDocs, doc, updateDoc, deleteField } from '../database/firebase.js';
+import { db, addDoc, collection, getDocs  } from '../database/firebase.js';
 
 const cards = document.getElementById('card-teplate');
-
 
 //Funcion para obtener todos las naves
 async function ListarNaves() {
   const querySnapshot = await getDocs(collection(db, "naves"));
   querySnapshot.forEach((item) => {
     cards.innerHTML += `
-      <div class="row-sm-12 col-md-4 row-lg-4 py-3">
-        <div class="card shadow">
-          <div class="card-body">
-            <h2>${item.data().nombre}</h2><hr>
-            <p class="text-center">constructor: ${item.data().nombre}</p>
-            <p class="text-center">Tipo: ${item.data().tipo}</p>
-            <p class="text-center">Primer Lanzamiento: ${item.data().planzamiento}</p>
-            <p class="text-center">Ultimo Lanzamiento: ${item.data().ultilanzamiento}</p>
-            <p class="text-center">Version: ${item.data().version}</p>
-            <a href="ver-naves.html" class="btn btn-sm btn-dark ">Ver Naves</a>
-            <a href="crear-nave.html" class="btn btn-sm btn-dark ">Crear Nave</a>
-          </div>
-        </div>
+    <div class="card">
+      <div class="card-header">
+        <h4>${item.data().nombre}</h4>
       </div>
+      <div class="card-body">
+      <p class="text-center">Tipo: ${item.data().tipo}</p>
+      <p class="text-center">Primer Lanzamiento: ${item.data().planzamiento}</p>
+      <p class="text-center">Ultimo Lanzamiento: ${item.data().ultilanzamiento}</p>
+      <p class="text-center">Version: ${item.data().version}</p>
+      </div>
+      <div class="card-footer">
+      <a href="#" class="btn btn-sm btn-dark ">Eliminar</a>
+      <a href="#" class="btn btn-sm btn-dark ">Actualizar</a>
+      </div>
+    </div>
     `;
   })
 }
 
 ListarNaves();
+
 
 
 //Funcion para crear las naves
