db69afe6f7312c0bef1cf0309d3c40673e055a51
luis_mendoza <luis.mendoza.alonso@hotmail.com>
2022-07-14 03:18:21 -0500

    Se crea la función ver todas las naves creadas en la base de datos.

--- js/index.js
+++ js/index.js
@@ -1,16 +1,32 @@
 import { db, getFirestore, addDoc, collection, getDocs, doc, updateDoc, deleteField } from '../database/firebase.js';
 
+const cards = document.getElementById('card-teplate');
 
 
 //Funcion para obtener todos las naves
-export async function ListarNaves() {
+async function ListarNaves() {
   const querySnapshot = await getDocs(collection(db, "naves"));
   querySnapshot.forEach((item) => {
-    console.log(`${item.id} => ${item.data().born}`);
-   
+    cards.innerHTML += `
+      <div class="row-sm-12 col-md-4 row-lg-4 py-3">
+        <div class="card shadow">
+          <div class="card-body">
+            <h2>${item.data().nombre}</h2><hr>
+            <p class="text-center">constructor: ${item.data().nombre}</p>
+            <p class="text-center">Tipo: ${item.data().tipo}</p>
+            <p class="text-center">Primer Lanzamiento: ${item.data().planzamiento}</p>
+            <p class="text-center">Ultimo Lanzamiento: ${item.data().ultilanzamiento}</p>
+            <p class="text-center">Version: ${item.data().version}</p>
+            <a href="ver-naves.html" class="btn btn-sm btn-dark ">Ver Naves</a>
+            <a href="crear-nave.html" class="btn btn-sm btn-dark ">Crear Nave</a>
+          </div>
+        </div>
+      </div>
+    `;
   })
 }
 
+ListarNaves();
 
 
 //Funcion para crear las naves
@@ -18,17 +34,24 @@ const formCrear = document.querySelector("#form-crear");
 formCrear.addEventListener("submit", async (e) => {
   e.preventDefault();
   const nombre = document.querySelector("#cnombre").value;
-  const tipo = document.querySelector("#ctipo").value;
-  const combustible = document.querySelector("#ccombustible").value;
-  const capacidad = document.querySelector("#ccapacidad").value;
-  const pais = document.querySelector("#cpais").value;
-  const docRef = await addDoc(collection(db, "naves"), {
-    nombre,
-    tipo,
-    combustible,
-    capacidad,
-    pais
-  });
+  const version = document.querySelector("#cversion").value;
+  const planzamiento = document.querySelector("#cplanzamiento").value;
+  const ultilanzamiento = document.querySelector("#cultilanzamiento").value;
+  const constructor = document.querySelector("#cconstructor").value;
+  const tipo = document.querySelector("#inputGroupSelect01").value;
+
+  const docRef = await addDoc(collection(db, "naves"),
+    {
+      nombre,
+      version,
+      planzamiento,
+      ultilanzamiento,
+      constructor,
+      tipo
+    });
   console.log("Document written with ID: ", docRef.id);
   formCrear.reset();
 });
+
+
+
